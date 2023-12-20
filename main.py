6da31c26fe0d83017f8058e934c02c1ab50b6621
davidArce <davidsteven654@gamil.com>
2023-12-20 15:24:34 -0500

    estados por jugador

--- main.py
+++ main.py
@@ -24,20 +24,23 @@ def main():
     # black_circle_positions = [[155, 100], [225, 100], [295, 100], [295, 190], [295, 145], [155, 145], [155, 190], [155, 235], [155, 280], [155, 325], [225, 145], [225, 190]]
     # red_circle_positions = [[365, 235], [365, 190], [365, 145], [505, 280], [365, 325], [435, 325], [505, 325], [435, 145], [435, 190], [435, 235], [435, 280], [365, 280]]
     
-    coordinates = [[85, 100], [85, 145], [85, 190], [85, 235], [85, 280], [85, 325], [155, 100], [155, 145], [155, 190], [155, 235], [155, 280], [155, 325], [225, 100], [225, 145], [225, 190], [225, 235], [225, 280], [225, 325], [295, 100], [295, 145], [295, 190], [295, 235], [295, 280], [295, 325], [365, 100], [365, 145], [365, 190], [365, 235], [365, 280], [365, 325], [435, 100], [435, 145], [435, 190], [435, 235], [435, 280], [435, 325], [505, 100], [505, 145], [505, 190], [505, 235], [505, 280], [505, 325], [575, 100], [575, 145], [575, 190], [575, 235], [575, 280], [575, 325]]
+    coordinates = [[85, 100], [155, 100], [225, 100], [295, 100], [365, 100], [435, 100], [505, 100], [575,100], [85, 145], [155, 145], [225, 145], [295, 145], [365, 145], [435, 145], [505, 145], [575, 145], [85, 190], [155, 190], [225, 190], [295, 190], [365, 190], [435, 190], [505, 190], [575, 190], [85, 235], [155, 235], [225, 235], [295, 235], [365, 235], [435, 235], [505, 235], [575, 235], [85, 280], [155, 280], [225, 280], [295, 280], [365, 280], [435, 280], [505, 280], [575, 280],[85, 325], [155, 325], [225, 325], [295, 325], [365, 325], [435, 325], [505, 325], [575, 325]]
     
     posicion_utilidad = [[85, 370], [155, 370], [225, 370], [295, 370], [365, 370], [435, 370], [505, 370], [575, 370]]
    
     
-    # Convertir a matriz de 8x6
+    # Convertir a matriz de 8x6 las coordenadas permitidas
     matrix = [coordinates[i:i + 8] for i in range(0, len(coordinates), 8)]
-    
     selected_circle = None
-    arr_cant_fichas = [0] * 8 #lista que almacena la cantidad de fichas en cada columna
-    # for fila in matrix:
-    #     for coordenada in fila:
-    #         print(coordenada)
-        
+    # num_filas = len(matrix)
+    # num_columnas = len(matrix[0]) if num_filas > 0 else 0
+    # for col_index in range(num_columnas):
+    #     for fila_index in range(num_filas):
+    #         item = matrix[fila_index][col_index]
+    #         print(item)
+    
+    # print(matrix)
+    
     #Agregar las posiciones de los círculos negros horizontalmente
     for x in range(155, 575 + 1, 70):
         black_circle_positions.append([x, 100])
@@ -85,37 +88,47 @@ def main():
             mostrar_mensaje(screen, mensaje, posicion_mensaje)
     
     #verifica si el juego termina
-    def termina(black_circle_positions, red_circle_positions, coordinates):
+    def termina(black_circle_positions, red_circle_positions):
         cant_fichas_negras = 0
         #comprobar si termina el juego
-        for i in range(8):
-            for item in coordinates[i * 6: (i + 1) * 6]:
+        num_filas = len(matrix)
+        num_columnas = len(matrix[0]) if num_filas > 0 else 0
+        
+        for col_index in range(num_columnas):
+            for fila_index in range(num_filas):
+                item = matrix[fila_index][col_index]
                 if item in black_circle_positions:
                     cant_fichas_negras += 1
                 elif item in red_circle_positions:
                     cant_fichas_negras = 0
             if cant_fichas_negras == 12:
                 return True
-            else:
-                return False
+        if cant_fichas_negras != 12:
+            return False
         
-    def suma_columna(black_circle_positions, red_circle_positions, arr_cant_fichas):
+    def suma_columna(black_circle_positions, red_circle_positions):
+        arr_cant_fichas = [0] * 8 #lista que almacena la cantidad de fichas en cada columna
         #contar y sumar la cantidad de fichas en cada columna
-        for i in range(8):
+        num_filas = len(matrix)
+        num_columnas = len(matrix[0]) if num_filas > 0 else 0
+        
+        # Recorrer la matriz por columnas
+        for col_index in range(num_columnas):
             cant_fichas = 0
-            # print(f"columna{i + 1}")
-            for item in coordinates[i * 6: (i + 1) * 6]:
+            for fila_index in range(num_filas):
+                item = matrix[fila_index][col_index]
                 if item in black_circle_positions:
                     cant_fichas += 1
                 elif item in red_circle_positions:
                     cant_fichas += 1
                 
-            arr_cant_fichas[i] = cant_fichas
+            arr_cant_fichas[col_index] = cant_fichas
         return arr_cant_fichas    
     
     turno_maquina = False
     while True:
-        print("turno humano")
+        
+        # print("turno humano")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -135,17 +148,27 @@ def main():
                             break
                     
                     # Si no hay colisión, mueve el círculo
-                    if not collision:                    
-                        for item in coordinates:
-                            if item[0] - 15 < mouse_pos[0] < item[0] + 15 and item[1] - 15 < mouse_pos[1] < item[1] + 15:
-                                selected_circle[0], selected_circle[1] = item
-                                turno_maquina = True
-                        # selected_circle[0], selected_circle[1] = mouse_pos
+                    if not collision:
+                        # index = 0                    
+                        for fila in matrix:
+                            index = 0
+                            for item in fila:
+                                if item[0] - 15 < mouse_pos[0] < item[0] + 15 and item[1] - 15 < mouse_pos[1] < item[1] + 15:
+                                    selected_circle[0], selected_circle[1] = item
+                                    cant_movimientos = suma_columna(black_circle_positions, red_circle_positions)
+                                    turno = cant_movimientos[index] - 1
+                                    
+                                    turno_maquina = True
+                                 
+                                index += 1
+                            # selected_circle[0], selected_circle[1] = mouse_pos
                         selected_circle = None
+
+                    # print(turno)
+            
     
         if turno_maquina:
-            print("turno maquina")
-            
+            # print("turno maquina")
             ficha_negra_aleatoria = random.choice(black_circle_positions)
             coordenada_aleatoria = random.choice(coordinates)
             if coordenada_aleatoria not in black_circle_positions + red_circle_positions:
@@ -153,9 +176,9 @@ def main():
                 turno_maquina = False
                 
             
-        cant_fichas = suma_columna(black_circle_positions, red_circle_positions, arr_cant_fichas)
+        cant_fichas = suma_columna(black_circle_positions, red_circle_positions)
         
-        termina(black_circle_positions, red_circle_positions, coordinates)
+        termina(black_circle_positions, red_circle_positions)
         
         board(black_circle_positions, red_circle_positions, cant_fichas)
 
